25bbf244a1ec70611e67a938353d9492605e3721
bin4ryio <haolun0905@gmail.com>
2017-08-17 18:39:31 +0800

    Moved authentication under API v1 namespace.

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,17 @@
 Rails.application.routes.draw do
-  resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  mount_devise_token_auth_for 'User', at: 'auth'
+  # mount_devise_token_auth_for 'User', at: 'auth'
   
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   
   namespace :api do
     namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+      resources :clubs
       resources :events
+      resources :items
     end
   end
-  
-  # constraints subdomain: 'api' do
-  #   scope module: 'api' do
-  #     namespace :v1 do
-  #       resources :events
-  #     end
-  #   end
-  # end
-
-  # namespace :api, path: '/' do
-  #   scope module: :v1 do
-  #     resources :events
-  #   end
-  # end
   
 end
